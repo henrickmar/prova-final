6588b8ff9c5e75ec99379cc63f8dc56fc1fc8659
Fulano <marcelohenrick812@gmail.com>
2023-11-29 19:47:03 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let cont = 1;
+
+setInterval(function () {
+    removerImagem();
+    proximaImagem();
+}, 2000);
+
+function removerImagem(){
+    document.getElementById("slider1").style.display = "none";
+    document.getElementById("slider2").style.display = "none";
+    document.getElementById("slider3").style.display = "none";
+}
+
+function proximaImagem(){
+    cont++;
+    
+    if(cont > 2){
+        cont = 1;
+    }
+    
+    let slider = document.getElementById("slider" + cont);
+    
+    slider.style.display = "flex";
+}
